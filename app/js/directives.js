b903884242964850488655aec004ac11fb8516ce
Igor Yarosh <IgorYarosh@coherentsolutions.com>
2016-02-22 18:11:51 +0300

    task 2 lazy init

--- app/js/directives.js
+++ app/js/directives.js
@@ -9,7 +9,9 @@ taskDirectives.directive('myTabs', function() {
     restrict: 'E',
     transclude: true,
     scope: {},
-    controller: ['$scope', function($scope) {
+    controller: ['$scope','$sce', function($scope,$sce) {
+      var content = $scope.content = $sce.trustAsHtml("<p>text</p>");
+
       var panes = $scope.panes = [];
 
       $scope.select = function(pane) {
@@ -17,17 +19,26 @@ taskDirectives.directive('myTabs', function() {
           pane.selected = false;
         });
         pane.selected = true;
+        $scope.content = pane.content;
         if (panes.length != 0){
         pane.action();
         
         }
       };
 
+      $scope.addPane = function(pane) {
+        var s = $sce.trustAsHtml(pane.content.context.innerHTML);
+        pane.content = s;
+            panes.push(pane);
+      };
+
+
+
       this.addPane = function(pane) {       
         if (panes.length === 0) {
-          $scope.select(pane);
+          $scope.select(pane);          
         }
-        panes.push(pane);
+          $scope.addPane(pane);
       };
     }],
     templateUrl: 'partials/my-tabs.html'
@@ -43,9 +54,20 @@ taskDirectives.directive('myPane', function() {
       title: '@',
       action: '&'
     },
-    link: function(scope, element, attrs, tabsCtrl) {
-      tabsCtrl.addPane(scope);
-    },
-    templateUrl: 'partials/my-pane.html'
+
+
+   link: function($scope, $element, $attrs, $ctrl, $transclude ) {
+      var d = $transclude(); 
+      $scope.content = d;
+      $ctrl.addPane($scope);
+            
+       
+      $transclude(function(clone){
+        //var pTag = angular.element(clone[1]);
+        //pTag.append(angular.element("<strong> - and to add something.</strong>"));        
+        //$element.append(clone);
+      });
+    }
+
   };
 });
