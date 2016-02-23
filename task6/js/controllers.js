f11cc65cf6ee86d655393dd5b0c0e9b00ac66ede
Igor Yarosh <IgorYarosh@coherentsolutions.com>
2016-02-24 00:33:32 +0300

    task6

--- task6/js/controllers.js
+++ task6/js/controllers.js
@@ -4,19 +4,12 @@
 
 var taskControllers = angular.module('taskControllers', []);
 
-taskControllers.controller('SetTimeOutController$', function($scope) {
+taskControllers.controller('SetTimeOutController$', function($scope,$timeout) {
     
-      $scope.getMessage = function() {
-        setTimeout(function() {
-          $scope.$apply(function() {
-            //wrapped this within $apply
-            $scope.message = 'Hello'; 
+    $timeout(function() {
+         $scope.message = 'Hello'; 
             console.log('message:' + $scope.message);
-          });
-        }, 2000);
-      }
-      
-      $scope.getMessage();
+    }, 2000);
     
     });
 
@@ -24,8 +17,11 @@ taskControllers.controller('SetTimeOutController', function($scope) {
     
       $scope.getMessage = function() {
         setTimeout(function() {
-          $scope.message = 'Hello';
-          console.log('message:'+$scope.message);
+          $scope.$apply(function() {
+            //wrapped this within $apply
+            $scope.message = 'Hello2'; 
+            console.log('message:' + $scope.message);
+          });
         }, 2000);
       }
       
@@ -46,11 +42,56 @@ taskControllers.controller('ClickController$', function($scope) {
 taskControllers.controller('ClickController', function($scope) {
     
       $scope.say = function() {        
-          $scope.message = 'Hello';
-          console.log('message:'+$scope.message);       
+          //$scope.$apply(function() {
+	          $scope.message = 'Hello3';
+	          console.log('message:'+$scope.message); 
+
+          //});    
       }
     
     });
+
+
+ 
+
+   function say(){
+    alert("adssa");
+   }
+
+taskControllers.controller('AjaxController$', function($scope,$http,$window) {
+    
+      $scope.saydd = function() {        
+         $http({
+        method: 'GET',
+        url: 'http://localhost:8000/task6/json/hello.json'       
+     }).success(function(data){       
+        $window.alert(JSON.stringify(data));
+    }).error(function(){
+        $window.alert("error");
+    });          
+      }
+    
+    });
+
+taskControllers.controller('AjaxController', function($scope,$http,$window) {
+    
+      $scope.saydd = function() {        
+         $http({
+        method: 'GET',
+        url: 'http://localhost:8000/task6/json/hello.json'       
+     }).success(function(data){       
+        $window.alert("hello "+ data);
+    }).error(function(){
+        $window.alert("error ");
+    });          
+      }
+    
+    });
+
+      
+
+
+
 
 
 
