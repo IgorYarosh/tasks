284e23ae79a94e84630bddfed6166e1c39c6edd8
Igor Yarosh <IgorYarosh@coherentsolutions.com>
2016-02-24 14:28:14 +0300

    TASK7

--- task7/js/script.js
+++ task7/js/script.js
@@ -1,23 +1,31 @@
 $(function() {
 
        $.widget( "custom.igorWidget", {
-     
-        // Default options.
-        options: {
-            value: 0,
+            
+        options: {           
             files : [ "file1","file2","file3" ]
         },
 
         _create: function() {
-
-             var files = this.options.files;
-
+             var files = this.options.files;             
+             $("<div id=\"dialog\" style=\"display: none;\"></div>").appendTo($( "body"));
              for(var i = 0; i < files.length;i++){    
                 $( "<div><input type=\"checkbox\"/>&nbsp;"+files[i]+"</div>" ).appendTo($( "#dialog"))
              }
-
               $( "#dialog").dialog(); 
-        }
+        },
+
+        setValue: function( value ) {         
+           console.log("set value " + value);
+        },
+
+        getValue: function(  ) {
+           console.log("get value ");
+        },
+
+        onValueChanged: function(  ) {
+           console.log("onValueChanged ");
+        },
 
         });
    
